8dd9af8e4ed52f8d18792cd7af80d0d62281ec06
Lucas Gomes Da Silva <1018126@sga.pucminas.br>
2020-11-21 00:42:53 -0300

    fix/porta padrão para o heroku

--- back-end/main.py
+++ back-end/main.py
@@ -22,6 +22,6 @@ def newAutorizedVoice():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 33507))
-    app.run(host='0.0.0.0', debug=True, port=port)
+    app.run(host='0.0.0.0', port=port)
 
     
