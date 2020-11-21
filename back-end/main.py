83c065f94264b1d5c23cf40073ba7bc608727054
Lucas Gomes Da Silva <1018126@sga.pucminas.br>
2020-11-21 01:32:38 -0300

    fix/procfile

--- back-end/main.py
+++ back-end/main.py
@@ -20,8 +20,8 @@ def newAutorizedVoice():
 
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 33507))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 33507))
+#     app.run(host='0.0.0.0', port=port)
 
     
